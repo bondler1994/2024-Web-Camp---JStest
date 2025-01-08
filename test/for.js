95cf4d205a3750acbd61f02f1ad2d386f9846d89
Watson <bbcallyou2@gmail.com>
2025-01-08 11:36:45 +0800

    try 1+2+3~10 = x

--- test/for.js
+++ test/for.js
@@ -28,3 +28,22 @@ for (let i = 0; i <= arrNum.length; i++) {
 }
 
 console.log(takeEvenNum); //(5) [2, 4, 6, 8, 10]
+
+//1+2...+10 =
+let total = 0;
+let presentNum = "";
+
+for (let i = 0; i <= 10; i++) {
+    total += i;
+    //因爲初始值是0 所以判斷如果是0 那就只顯示 0 而不要＋符號
+    //排除0之後 剩下的值 都要加上＋符號 
+    presentNum += i === 0 ? `${i}` : ` + ${i}`;
+    // if(presentNum += i === 0){
+    //     return `${i}`
+    // }else{
+    //     return +`${i}`
+    // }
+}
+
+console.log(`${presentNum} = ${total}`);
+//0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
